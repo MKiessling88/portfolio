80d5b679ad83b02dd7239f22da49bfaad34f8292
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-05-27 12:20:52 +0200

    add image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { HeaderComponent } from "./shared/header/header.component";
+import { WhyMeComponent } from "./why-me/why-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
