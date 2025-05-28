a6a79c11d86d6ef6168984a864e5d1de2026aa1a
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-05-28 13:08:43 +0200

    edit my-skills component & add my-projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { HeaderComponent } from "./shared/header/header.component";
 import { WhyMeComponent } from "./why-me/why-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, WhyMeComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/my-skills/my-skills.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-my-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-skills.component.html',
+  styleUrl: './my-skills.component.scss'
+})
+export class MySkillsComponent {
+  skills = [
+    { imagePath: 'assets/img/skills/skill-Angular.png' },
+    { imagePath: 'assets/img/skills/skill-TypeScript.png' },
+    { imagePath: 'assets/img/skills/skill-JavaScript.png' },
+    { imagePath: 'assets/img/skills/skill-HTML.png' },
+    { imagePath: 'assets/img/skills/skill-CSS.png' },
+    { imagePath: 'assets/img/skills/skill-Rest-Api.png' },
+    { imagePath: 'assets/img/skills/skill-Firebase.png' },
+    { imagePath: 'assets/img/skills/skill-GIT.png' },
+    { imagePath: 'assets/img/skills/skill-Scrum.png' },
+    { imagePath: 'assets/img/skills/skill-MaterialDesign.png' },
+    { imagePath: 'assets/img/skills/skill-Challenge.png' },
+  ];
+}
