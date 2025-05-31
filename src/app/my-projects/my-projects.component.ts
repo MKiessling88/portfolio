f6b1aa08c0b40ebcaef244d1207fb9c13e59ea18
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-05-31 09:59:04 +0200

    add project component

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
+
+  public projects = [
+    {
+      "imagePath": "./../../../assets/img/tressures.png",
+      "name": "JOIN",
+      "technologies": "HTML | CSS | JavaScript | Firebase",
+      "GIT": "https://github.com/MKiessling88/Join-Gruppenarbeit",
+      "link": ""
+    }, {
+      "imagePath": "./../../../assets/img/tressures.png",
+      "name": "Treasures-of-Eternity",
+      "technologies": "HTML | CSS | JavaScript",
+      "GIT": "https://github.com/MKiessling88/Treasures-of-Eternity",
+      "link": ""
+    }
+  ];
 
 }
