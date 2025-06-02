70b87c728fe0a084b9928bd2b516312d2107fdfe
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-06-02 14:18:08 +0200

    add Privacy Policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent},
-    { path: 'LegalNotice', component: LegalNoticeComponent},
+    { path: '', component: MainContentComponent },
+    { path: 'LegalNotice', component: LegalNoticeComponent },
+    { path: 'PrivacyPolicy', component: PrivacyPolicyComponent },
 ];
